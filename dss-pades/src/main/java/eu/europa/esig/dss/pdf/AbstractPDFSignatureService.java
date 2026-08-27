94aff86ed3a97e26b97e92edace741f78299a6b8
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2026-08-27 10:28:59 +0200

    DSS-3951 : execute spotless (6.5)

--- dss-pades/src/main/java/eu/europa/esig/dss/pdf/AbstractPDFSignatureService.java
+++ dss-pades/src/main/java/eu/europa/esig/dss/pdf/AbstractPDFSignatureService.java
@@ -65,7 +65,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
